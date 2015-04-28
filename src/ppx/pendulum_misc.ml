fe2cb130d15b3a452690855a34a9b7b249f29ce9
remyzorg <besognet.remy@gmail.com>
2015-04-28 13:55:39 +0200

    version mandelthesis + remove Ast.statement pass t

---
+++ src/ppx/pendulum_misc.ml
@@ -0,0 +1,99 @@
+
+open Ast_mapper
+open Ast_helper
+open Asttypes
+open Parsetree
+open Longident
+
+open Pendulum_preproc
+
+
+
+let string_const ident =
+  let open Ast in
+  Exp.constant ~loc:ident.loc (Const_string (ident.content, None))
+
+
+let rec expr_of_ast e =
+  let open Ast in
+  let open Ast.Derived in
+  let rec visit e =
+    begin match e.content with
+    | Nothing ->
+      [%expr Nothing]
+
+    | Pause ->
+      [%expr Pause]
+
+    | Emit signal ->
+      [%expr Emit [%e string_const signal]]
+
+
+    | Loop e ->
+      [%expr Loop ([%e visit e])]
+
+    | Seq (e1, e2) ->
+      [%expr Seq ([%e visit e1], [%e visit e2])]
+
+    | Par (e1, e2) ->
+      [%expr Par ([%e visit e1], [%e visit e2])]
+
+    | Present (signal, e1, e2) ->
+      [%expr Present ([%e string_const signal], [%e visit e1], [%e visit e2])]
+
+    | Signal (signal, e) ->
+      [%expr Signal ([%e string_const signal], [%e visit e])]
+
+    | Suspend (e, signal) ->
+      [%expr Suspend ([%e visit e], [%e string_const signal])]
+
+    | Trap (Label label, e) ->
+      [%expr Trap (Label [%e string_const label], [%e visit e])]
+
+    | Halt ->
+      [%expr Halt]
+
+    | Sustain signal ->
+      [%expr Sustain ([%e string_const signal])]
+
+    | Present_then (signal, e) ->
+      [%expr Present_then ([%e string_const signal], [%e visit e])]
+
+    | Await signal ->
+      [%expr (Await [%e string_const signal])]
+
+    | Abort (e, signal) ->
+      [%expr Abort ([%e visit e], [%e string_const signal])]
+
+    | Loop_each (e, signal) ->
+      [%expr Loop_each ([%e visit e], [%e string_const signal])]
+
+    | Every (signal, e) ->
+      [%expr Every ([%e string_const signal], [%e visit e])]
+
+    | _ -> syntax_error ~loc:e.loc "Syntax error : pendulum keyword expected"
+    end  [@metaloc e.loc]
+  in visit e
+
+
+let print_to_dot loc =
+  let open Location in
+  let n = ref 0 in
+  fun e ->
+    let name = Filename.(basename @@ chop_extension @@
+                         loc.loc_start.Lexing.pos_fname) ^ ("_" ^ (string_of_int !n)) in
+    let fgname = (name ^ "_fg") in
+    let tagname = (name ^ "_tast") in
+    (* let cwd = Sys.getcwd () in *)
+    (* Format.printf "FILE HERE : |%s| |%n| |%s|" name (incr n; !n) cwd; *)
+    let c_tagged = open_out (tagname ^ ".dot") in
+    let c_flowgraph = open_out (fgname ^ ".dot")in
+    let fmt_tagged = Format.formatter_of_out_channel c_tagged in
+    let fmt_flowgraph = Format.formatter_of_out_channel c_flowgraph in
+    Ast.Tagged.print_to_dot fmt_tagged e;
+    Grc.Flowgraph.print_to_dot fmt_flowgraph (Grc.flowgraph e);
+    close_out c_flowgraph; close_out c_tagged;
+    ignore @@ Sys.command (Format.sprintf "dot -Tpdf %s.dot -o %s.pdf" tagname tagname);
+    ignore @@ Sys.command (Format.sprintf "dot -Tpdf %s.dot -o %s.pdf" fgname fgname);
+    Unix.unlink (tagname ^ ".dot");
+    Unix.unlink (fgname ^ ".dot")
---
+++ src/preproc/grc.mli
@@ -0,0 +1,49 @@
+
+
+
+module Selection_tree : sig
+
+  type t
+
+  module Primitive : sig
+    val exit : t -> unit
+    val enter : t -> unit
+    val sync : t -> bool
+  end
+
+
+end
+
+module Flowgraph : sig
+
+  type action =
+    | Emit of string [@printer fun fmt -> Format.fprintf fmt "%s"]
+    | Atom of Parsetree.expression
+    | Enter of int
+    | Exit of int
+    | Finish
+    | SetFinish of bool
+
+  type test_value =
+    | Signal of string [@printer fun fmt -> Format.fprintf fmt "%s"]
+    | Selection of int
+    | Finished
+
+  type node =
+    | Call of action
+    | Test of test_value
+    | Sync of int * int
+    | Fork
+    | Dep
+
+  type t =
+    | Node_bin of node * t * t
+    | Node of node * t
+    | Leaf of node
+
+  val print_to_dot : Format.formatter -> t -> unit
+
+end
+
+val flowgraph : Ast.Tagged.t -> Flowgraph.t
+val of_ast : Ast.Tagged.t -> Selection_tree.t * Flowgraph.t
