e2052bd63afc23601ea94fb9899302a2ac722ebc
remy <besognet.remy@gmail.com>
2015-07-21 15:54:42 +0200

    Advanced js_example

--- examples/mouse_loc_js/main.ml
+++ examples/mouse_loc_js/main.ml
@@ -2,22 +2,46 @@
 open Firebug
 
 let error f = Printf.ksprintf
-    (fun s -> Firebug.console##.error (Js.string s); failwith s) f
+    (fun s -> Firebug.console##error (Js.string s); failwith s) f
 let debug f = Printf.ksprintf
-    (fun s -> Firebug.console##.log(Js.string s)) f
+    (fun s -> Firebug.console##log(Js.string s)) f
 let alert f = Printf.ksprintf
-    (fun s -> Dom_html.window##.alert(Js.string s); failwith s) f
+    (fun s -> Dom_html.window##alert(Js.string s); failwith s) f
 
 let (@>) s coerce =
   Js.Opt.get (coerce @@ Dom_html.getElementById s)
     (fun () -> error "can't find element %s" s)
 
+let%sync mouse_machine =
+  input tarea;
+  input move;
 
+  loop begin
+    present move (
+      atom (
+        (!!tarea)##.textContent := Js.string @@
+          Format.sprintf "%dx%d" (fst !!move) (snd !!move)
+      ));
+    pause
+  end
 
 let _ =
   let open Dom_html in
   window##.onload := handler (fun _ ->
-      let area = "tarea" @> CoerceTo.textarea in
+      let area = "tarea" @> CoerceTo.a in
+      area##.textContent := Js.string "lol";
+      (* let c = "canvas" @> CoerceTo.canvas in *)
+      (* let ctx = canvas##getContext (Dom_html._2d_) in *)
+
+      let (set_tarea, set_move), step =
+        mouse_machine (area, (0,0))
+      in
+
+      window##.onmousemove := handler (fun e ->
+          set_move (e##.clientX, e##.clientY);
+          ignore @@ step ();
+          Js._false);
+
       Js._false
     )
 
