076be2bde18a838e8ff9c7a41e4f05dabc930ad4
remy <besognet.remy@gmail.com>
2016-05-28 15:54:38 +0200

    update mouse_loc

--- examples/mouse_loc_js/main.ml
+++ examples/mouse_loc_js/main.ml
@@ -1,51 +1,28 @@
+open Dom_html
+open Format
 
-open Firebug
-
-let error f = Printf.ksprintf
-    (fun s -> Firebug.console##error (Js.string s); failwith s) f
-let debug f = Printf.ksprintf
-    (fun s -> Firebug.console##log(Js.string s)) f
-let alert f = Printf.ksprintf
-    (fun s -> Dom_html.window##alert(Js.string s); failwith s) f
-
-let (@>) s coerce =
-  Js.Opt.get (coerce @@ Dom_html.getElementById s)
-    (fun () -> error "can't find element %s" s)
-
-let iter opt f =
-  match opt with
-  | None -> ()
-  | Some o -> f o
-
-
-
-let%sync mouse_react =
-  input span;
+let%sync mouse ~obj =
+  input s;
   input w {
-    onmousemove = "", (fun x ev ->
-        Format.sprintf "%d,%d" ev##.clientX ev##.clientY);
+    onmousemove =
+      "",
+      (fun x ev -> sprintf "%d,%d" ev##.clientX ev##.clientY);
   };
 
   loop begin
-    present w##onmousemove (
-      emit span##.textContent
-        (Js.(some (string !!(w##onmousemove))))
-    ); pause
+    present w##onmousemove begin
+      emit s##.textContent (Js.(some (string !!(w##onmousemove))))
+    end;
+    pause
   end
+;;
 
+let onload _ =
+  let s = createSpan document in
+  Dom.appendChild document##.body s;
+  mouse (s, window);
+  Js._false
+;;
 
-
-let _ =
-  let open Dom_html in
-
-  window##.onkeydown := handler (fun _ ->
-      debug "lol";
-      Js._false
-    );
-
-  window##.onload := handler (fun _ ->
-      let area = "tarea" @> CoerceTo.a in
-      let _step = mouse_react (area, window) in
-      Js._false
-    )
+window##.onload := handler onload;;
 
