f0650afc90637583d9c4613a8555074d14106048
remyzorg <besognet.remy@gmail.com>
2018-03-23 17:51:57 +0100

    js map example

--- examples/js/maps_places/maps_places.js
+++ examples/js/maps_places/maps_places.js
@@ -1,33 +1,76 @@
 var map;
 var infowindow;
+let current_marker;
 
-function initMap() {
-    var pyrmont = {lat: -33.867, lng: 151.195};
+let lat;
+let lng;
 
+let my_loc;
+
+var loc_options = {
+    // enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+};
+
+function createMarker () {
+    var myloc = new google.maps.Marker({
+        clickable: false,
+        icon: new google.maps.MarkerImage(
+            '//maps.gstatic.com/mapfiles/mobile/mobileimgs2.png',
+            new google.maps.Size(22,22),
+            new google.maps.Point(0,18),
+            new google.maps.Point(11,11)),
+        shadow: null,
+        zIndex: 999,
+        map
+    });
+    return myloc;
+}
+
+function position_first (position) {
+    lat = position.coords.latitude;
+    lng = position.coords.longitude;
+
+    map.setCenter({lat, lng});
+    myloc.setPosition({lat, lng});
+}
+
+function refresh_current (position) {
+    lat = position.coords.latitude;
+    lng = position.coords.longitude;
+
+    myloc.setPosition({lat, lng});
+
+    console.log(position.coords.latitude + " ",
+                position.coords.longitude  + " ",
+                position.coords.accuracy);
+
+
+
+}
+
+function refresh_error (err) {
+    console.log("Error on location");
+}
+
+
+function initMap () {
     map = new google.maps.Map(document.getElementById('map'), {
-        center: pyrmont,
         zoom: 15
     });
 
     infowindow = new google.maps.InfoWindow();
+    myloc = createMarker ();
 
-    var service = new google.maps.places.PlacesService(map);
-    service.nearbySearch({
-        location: pyrmont,
-        radius: 500,
-        types: ['store']
-    }, callback);
+    navigator.geolocation.getCurrentPosition(position_first, refresh_error, loc_options);
+    navigator.geolocation.watchPosition(refresh_current, refresh_error, loc_options);
+
+    init ();
+
 }
 
-function callback(results, status) {
-    if (status === google.maps.places.PlacesServiceStatus.OK) {
-        for (var i = 0; i < results.length; i++) {
-            createMarker(results[i]);
-        }
-    }
-}
-
-function createMarker(place) {
+function add_marker_to_map(place) {
     var placeLoc = place.geometry.location;
     var marker = new google.maps.Marker({
         map: map,
@@ -39,3 +82,40 @@ function createMarker(place) {
         infowindow.open(map, this);
     });
 }
+
+function init() {
+
+    var searchBox = new google.maps.places.SearchBox(document.getElementById('pac-input'));
+    map.controls[google.maps.ControlPosition.TOP_CENTER].push(document.getElementById('pac-input'));
+    google.maps.event.addListener(searchBox, 'places_changed', function() {
+        searchBox.set('map', null);
+
+
+        var places = searchBox.getPlaces();
+
+        var bounds = new google.maps.LatLngBounds();
+        var i, place;
+        for (i = 0; place = places[i]; i++) {
+            (function(place) {
+                var marker = new google.maps.Marker({
+                    position: place.geometry.location
+                });
+                marker.bindTo('map', searchBox, 'map');
+                google.maps.event.addListener(marker, 'map_changed', function() {
+                    if (!this.getMap()) {
+                        this.unbindAll();
+                    }
+                });
+                bounds.extend(place.geometry.location);
+
+
+            }(place));
+
+        }
+        map.fitBounds(bounds);
+        searchBox.set('map', map);
+        map.setZoom(Math.min(map.getZoom(),17));
+
+    });
+}
+// google.maps.event.addDomListener(window, 'load', init);
